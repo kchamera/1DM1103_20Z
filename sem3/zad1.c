0390fc2174bc4a847eded382c1554e924242dc94
kacper <kacper@MacBook-Air-kacper.local>
2020-10-22 16:13:21 +0200

    as

---
+++ sem3/zad1.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  zad1
+//
+//  Created by kacper on 22/10/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
